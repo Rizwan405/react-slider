9337a2509a7f577d9b0c67bdce525cdbab20d48a
Rizwan <rizwanzaheer405@gmail.com>
2024-07-14 21:36:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from "./Carousel";
+import SlickyCarousal from "./SlickyCarousal";
+
+const App = () => {
+  return (
+    <main>
+      {/* <Carousel /> */}
+      <SlickyCarousal />
+    </main>
+  );
+};
+export default App;
